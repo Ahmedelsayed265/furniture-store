01ba2e19ad6147d977533bdbb7f7c33ef90de61a
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-15 17:48:49 +0200

    1

--- src/components/navigation/nav.jsx
+++ src/components/navigation/nav.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./nav.css";
 import smStar from "./../assest/smStar.png";
 class Nav extends Component {
@@ -9,8 +10,24 @@ class Nav extends Component {
           <div className="nav_header">
             <div className="left_head">
               <img src={smStar} alt="" />
+              <span>10% discount for registered users</span>
             </div>
-            <div className="right_head"></div>
+            <div className="right_head">
+              <div className="d-flex align-items-center justify-content-center">
+                <Link to="#">
+                  <i className="fab fa-facebook-square"></i>
+                </Link>
+                <Link to="#">
+                  <i class="fab fa-instagram"></i>
+                </Link>
+                <Link to="#">
+                  <i class="fab fa-twitter"></i>
+                </Link>
+                <span>
+                  English <i class="fas fa-angle-down"></i>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </React.Fragment>
