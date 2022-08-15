70c80a6f36b1df86f167fe3f8f90302a3880cfb8
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-15 19:02:44 +0200

    1

--- src/components/navigation/nav.jsx
+++ src/components/navigation/nav.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
 import smStar from "./../assest/smStar.png";
+import EskilLogo from "./../assest/EskilLogo.png";
 class Nav extends Component {
   render() {
     return (
@@ -18,17 +19,61 @@ class Nav extends Component {
                   <i className="fab fa-facebook-square"></i>
                 </Link>
                 <Link to="#">
-                  <i class="fab fa-instagram"></i>
+                  <i className="fab fa-instagram"></i>
                 </Link>
                 <Link to="#">
-                  <i class="fab fa-twitter"></i>
+                  <i className="fab fa-twitter"></i>
                 </Link>
                 <span>
-                  English <i class="fas fa-angle-down"></i>
+                  English <i className="fas fa-angle-down"></i>
                 </span>
               </div>
             </div>
           </div>
+          <nav>
+            <div className="left_sup_nav d-flex align-items-center">
+              <Link to="#">
+                <img src={EskilLogo} alt="Logo" />
+              </Link>
+              <ul>
+                <li>
+                  <Link to="#">HOME</Link>
+                </li>
+                <li>
+                  <Link to="#">ABOUT</Link>
+                </li>
+                <li>
+                  <Link to="#">CONTACT</Link>
+                </li>
+                <li>
+                  <Link to="#">Gallery</Link>
+                </li>
+                <li>
+                  <Link to="#">SHOP</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="right_sup_nav d-flex align-items-center">
+              <ul>
+                <li>
+                  <Link to="#">
+                    <i className="far fa-user"></i>Account
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#">
+                    <i className="far fa-heart"></i>Wishlist
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#">
+                    <ion-icon name="bag-outline"></ion-icon>
+                    Cart $0.00
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </div>
       </React.Fragment>
     );
