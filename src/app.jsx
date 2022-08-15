78de5bea7c8eec4dbfd2ae9288755f9bf3308a84
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-15 15:31:30 +0200

    1

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,9 @@
+import React, { Component } from "react";
+class App extends Component {
+  state = {};
+  render() {
+    return <h1>furniture store</h1>;
+  }
+}
 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app";
 
-ReactDOM.render(<h1>APP</h1>, document.querySelector("#root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
