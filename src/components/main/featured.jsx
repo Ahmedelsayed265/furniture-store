906c4c5c5ccc061b931d98a24b4ff9931c69d35b
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-16 23:25:13 +0200

    1

--- src/components/main/featured.jsx
+++ src/components/main/featured.jsx
@@ -7,6 +7,10 @@ const FeaturedComponent = (props) => {
         <div className="inner_card">
           <div className="img_wraper">
             <img src={props.product.url} alt="" />
+            <div className="menu">
+            <span><i className="fas fa-eye"></i></span>
+            <span><i className="far fa-heart"></i></span>
+            </div>
           </div>
           <div className="img_caption">
             <p>{props.product.name}</p>
