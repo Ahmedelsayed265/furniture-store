b827a6d28cad0b5e4480734f28668a4b47384884
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-16 21:25:30 +0200

    1

--- src/Parent/app.jsx
+++ src/Parent/app.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import axios from "axios";
 import Nav from "./../components/navigation/nav";
 import HomeComp from "./../components/main/home";
 import "./app.css";
 
 class App extends Component {
-  state = {};
+  state = {
+    products: [],
+  };
+  async componentDidMount() {
+    let { data } = await axios.get("http://localhost:3000/products");
+    this.setState({ products: data });
+  }
   render() {
     return (
       <React.Fragment>
         <Nav />
         <div className="main_entire">
           <Switch>
-            <Route path="/home" component={HomeComp} />
+            <Route
+              path="/home"
+              render={(props) => (
+                <HomeComp
+                  products={this.state.products.filter((p) => p.featured)}
+                  {...props}
+                />
+              )}
+            />
           </Switch>
         </div>
       </React.Fragment>
--- src/components/main/home.jsx
+++ src/components/main/home.jsx
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import FeaturedComponent from "./featured";
 
 import "./home.css";
 import EskilMP4 from "./../assest/EskilMP4.mp4";
@@ -12,110 +13,110 @@ import brand4 from "./../assest/brand4.png";
 import brand5 from "./../assest/brand5.png";
 import brand6 from "./../assest/brand6.png";
 
-class HomeComp extends Component {
-  state = {};
-  render() {
-    return (
-      <React.Fragment>
-        <div className="row header_content">
-          <div className="col-4 up_1">
-            <div className="sitetitle">
-              <div className="head">
-                <h1>
-                  FURNITURE & <br /> DÉCOR
-                </h1>
-                <p>
-                  Made from premium non-stretch Japanese denim for a
-                  vintage-inspired look, the ’90s Cheeky Jean has an easy
-                  straight leg.
-                </p>
-              </div>
-              <div className="read_more">
-                <Link to="/about">Learn More</Link>
-              </div>
-              <div className="quick_access">
-                <ul>
-                  <li>
-                    <Link to="/cart">
-                      <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                      chair
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/cart">
-                      <i class="fa-solid fa-arrow-up-right-from-square"></i>home
-                      decor
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/cart">
-                      <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                      tabels
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/cart">
-                      <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                      lighting
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="col-5 up_1 p-0 vid_entire">
-            <div className="sm-caption">
-              <div className="caption">
-                <p>10% discount for registered users</p>
-              </div>
-              <img src={smStar} alt="star" />
-            </div>
-            <video loop muted autoPlay playsInline src={EskilMP4} />
-          </div>
-          <div className="col-3 up_1 p-0 ">
-            <div className="last_container d-flex flex-column">
-              <img src={SMChair} alt="chair" />
+const HomeComp = (props) => {
+  return (
+    <React.Fragment>
+      <div className="row header_content">
+        <div className="col-4 up_1">
+          <div className="sitetitle">
+            <div className="head">
+              <h1>
+                FURNITURE & <br /> DÉCOR
+              </h1>
               <p>
-                The perfect place for every contemporary furniture store and
-                manufacture. This is Eskil
+                Made from premium non-stretch Japanese denim for a
+                vintage-inspired look, the ’90s Cheeky Jean has an easy straight
+                leg.
               </p>
             </div>
+            <div className="read_more">
+              <Link to="/about">Learn More</Link>
+            </div>
+            <div className="quick_access">
+              <ul>
+                <li>
+                  <Link to="/cart">
+                    <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                    chair
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/cart">
+                    <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                    home decor
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/cart">
+                    <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                    tabels
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/cart">
+                    <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                    lighting
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="row brands_wrap">
-          <div className="brands">
-            <div>
-              <img src={brand1} alt="brand1" />
+        <div className="col-5 up_1 p-0 vid_entire">
+          <div className="sm-caption">
+            <div className="caption">
+              <p>10% discount for registered users</p>
             </div>
-            <div>
-              <img src={brand2} alt="brand2" />
-            </div>
-            <div>
-              <img src={brand5} alt="brand5" />
-            </div>
-            <div>
-              <img src={brand6} alt="brand6" />
-            </div>
-            <div>
-              <img src={brand3} alt="brand3" />
-            </div>
-            <div>
-              <img src={brand4} alt="brand4" />
-            </div>
+            <img src={smStar} alt="star" />
+          </div>
+          <video loop muted autoPlay playsInline src={EskilMP4} />
+        </div>
+        <div className="col-3 up_1 p-0 ">
+          <div className="last_container d-flex flex-column">
+            <img src={SMChair} alt="chair" />
+            <p>
+              The perfect place for every contemporary furniture store and
+              manufacture. This is Eskil
+            </p>
           </div>
         </div>
-        <div className="row featured">
-          <div className="col-12 mb-5">
-            <p className="text-center mb-1 mt-5">BROWSE OUR ITEMS</p>
-            <h2 className="text-center">Ideal for your home</h2>
-            <div className="line">
-              <span></span>
-            </div>
+      </div>
+      <div className="row brands_wrap">
+        <div className="brands">
+          <div>
+            <img src={brand1} alt="brand1" />
+          </div>
+          <div>
+            <img src={brand2} alt="brand2" />
+          </div>
+          <div>
+            <img src={brand5} alt="brand5" />
+          </div>
+          <div>
+            <img src={brand6} alt="brand6" />
+          </div>
+          <div>
+            <img src={brand3} alt="brand3" />
+          </div>
+          <div>
+            <img src={brand4} alt="brand4" />
           </div>
         </div>
-      </React.Fragment>
-    );
-  }
-}
+      </div>
+      <div className="row featured">
+        <div className="col-12 mb-5">
+          <p className="text-center mb-1 mt-5">BROWSE OUR ITEMS</p>
+          <h2 className="text-center">Ideal for your home</h2>
+          <div className="line">
+            <span></span>
+          </div>
+        </div>
+        {props.products.map((product) => (
+          <FeaturedComponent />
+        ))}
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default HomeComp;
