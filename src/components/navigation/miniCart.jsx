cd64f94d5f4d6b4aa2431ad12a5dfa1d657ef429
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-19 20:56:19 +0200

    1

--- src/components/navigation/miniCart.jsx
+++ src/components/navigation/miniCart.jsx
@@ -1,41 +1,54 @@
-import React from "react";
-import "./miniCart.css";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./miniCart.css";
 import PRO from "./pro";
-const MINCART = (props) => {
-  return (
-    <div className={`mini_cart ${props.isCartActive ? "active" : ""}`}>
-      <div className="close">
-        <ion-icon onClick={props.closeCart} name="close-outline"></ion-icon>
-      </div>
-      <div className="cart_container">
-        <div className="head">
-          <h3>Shopping Cart</h3>
+import cart from "./../assest/cart.png";
+class MINCART extends Component {
+  cartEmpty() {
+    if (this.props.products.length !== 0) {
+      return this.props.products.map((product) => (
+        <PRO
+          product={product}
+          key={product.id}
+          onDelete={this.props.onDelete}
+        />
+      ));
+    } else {
+      return <img src={cart} alt="emptyCart" />;
+    }
+  }
+  render() {
+    const { isCartActive, closeCart, navReduce } = this.props;
+    return (
+      <div className={`mini_cart ${isCartActive ? "active" : ""}`}>
+        <div className="close">
+          <ion-icon onClick={closeCart} name="close-outline"></ion-icon>
         </div>
-        <div className="products">
-          {props.products.map((product) => (
-            <PRO product={product} key={product.id} onDelete={props.onDelete} />
-          ))}
-        </div>
-        <div className="Total">
-          <div className="total_inner">
-            <h4>Total</h4>
-            <span>${props.navReduce}</span>
+        <div className="cart_container">
+          <div className="head">
+            <h3>Shopping Cart</h3>
+          </div>
+          <div className="products">{this.cartEmpty()}</div>
+          <div className="Total">
+            <div className="total_inner">
+              <h4>Total</h4>
+              <span>${navReduce}</span>
+            </div>
           </div>
         </div>
+        <div className="Route_links">
+          <Link className="black_Link" to="/cart">
+            <ion-icon name="bag-outline"></ion-icon>
+            <span> VIEW SHOPPING CART</span>
+          </Link>
+          <Link to="/checkout" className="check">
+            GO TO CHECKOUT{" "}
+            <i className="fa-solid fa-arrow-up-right-from-square"></i>
+          </Link>
+        </div>
       </div>
-      <div className="Route_links">
-        <Link className="black_Link" to="/cart">
-          <ion-icon name="bag-outline"></ion-icon>
-          <span> VIEW SHOPPING CART</span>
-        </Link>
-        <Link to="/checkout" className="check">
-          GO TO CHECKOUT{" "}
-          <i className="fa-solid fa-arrow-up-right-from-square"></i>
-        </Link>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default MINCART;
