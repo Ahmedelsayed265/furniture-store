a41baf70dcf2fb6241f84f45eb0123fa5fb71fd9
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-25 15:34:14 +0200

    1

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -4,6 +4,7 @@ import "./contact.css";
 class Contact extends Component {
   state = {};
   render() {
+    const style = { width: "100%", height: "100%", frameborder: "0" };
     return (
       <React.Fragment>
         <div className="go_back">
@@ -11,7 +12,16 @@ class Contact extends Component {
         </div>
         <section className="contact">
           <h2>CONTACT US</h2>
-          <div className="for_map"></div>
+          <div className="for_map">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11678.8161
+              46884408!2d-81.219954!3d42.963441!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882ef24ff5ea0121%3A0xa
+              83d31167b636d38!2zMjAwIEFkZWxhaWRlIFN0IFMsIExvbmRvbiwgT04gTjVaIDNMMdiMINmD2YbYr9in!5e0!3m2!1sar!2sus!4v1661434285726!5m2!1sar!2sus"
+              style={style}
+              title="map"
+              aria-hidden="false"
+            ></iframe>
+          </div>
           <div className="info">
             <p>
               We are always keen to hear from people that would like to add a
