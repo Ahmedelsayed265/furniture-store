3b92e10250e0f3a87eaddfe7098235ac0c91d498
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-26 22:46:38 +0200

    1

--- src/components/shop/shopmenu.jsx
+++ src/components/shop/shopmenu.jsx
@@ -3,7 +3,18 @@ import { Link } from "react-router-dom";
 import "./shop.css";
 import ProductComp from "./product";
 class SHOP extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredProducts: props.products,
+    };
+  }
+  chairs = () => {
+    let filteredState = [...this.state.filteredProducts];
+    let products = this.props.products.filter((p) => p.category === "Chairs");
+    filteredState = products;
+    this.setState({ filteredProducts: filteredState });
+  };
   render() {
     return (
       <React.Fragment>
@@ -15,14 +26,51 @@ class SHOP extends Component {
             <h3>CATEGORIES</h3>
             <ul>
               <li>
-                <span>BEST SELLERS</span>
+                <span>
+                  <i className="fas fa-arrow-right"></i>ALL PRODUCTS
+                </span>
+              </li>
+              <li>
+                <span>
+                  <i className="fas fa-arrow-right"></i>BEST SELLERS
+                </span>
+              </li>
+              <li>
+                <span onClick={this.chairs}>
+                  <i className="fas fa-arrow-right"></i>CHAIRS
+                </span>
+              </li>
+              <li>
+                <span>
+                  <i className="fas fa-arrow-right"></i>LIGHTNING
+                </span>
+              </li>
+              <li>
+                <span>
+                  <i className="fas fa-arrow-right"></i>COFFE TABELS
+                </span>
+              </li>
+              <li>
+                <span>
+                  <i className="fas fa-arrow-right"></i>WOODEN CLOCKS
+                </span>
+              </li>
+              <li>
+                <span>
+                  <i className="fas fa-arrow-right"></i>SOFAS
+                </span>
+              </li>
+              <li>
+                <span>
+                  <i className="fas fa-arrow-right"></i>Gifts
+                </span>
               </li>
             </ul>
           </aside>
           <main>
             <h2>ALL PRODUCTS</h2>
             <div className="row">
-              {this.props.products.map((product) => (
+              {this.state.filteredProducts.map((product) => (
                 <ProductComp
                   key={product.id}
                   product={product}
