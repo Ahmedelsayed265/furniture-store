1df07eb95fc82f0cae75cd0451977dfb13b8192a
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-09-07 18:57:24 +0200

    1

--- src/components/navigation/pro.jsx
+++ src/components/navigation/pro.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./pro.css";
 const PRO = (props) => {
+  const { url2, name, id, count, price } = props.product;
   return (
     <div className="product">
       <div className="pro_img">
-        <img src={props.product.url2} alt={`img${props.product.id}`} />
+        <img src={url2} alt={`img${id}`} />
       </div>
       <div className="pro_detail">
-        <h6>{props.product.name}</h6>
+        <h6>{name}</h6>
         <p>
-          Count: <span>{props.product.count}</span>
+          Count: <span>{count}</span>
         </p>
-        <p>{(props.product.price * props.product.count).toFixed(2)}</p>
+        <p>{(price * count).toFixed(2)}</p>
       </div>
       <div className="del_pro">
         <i
